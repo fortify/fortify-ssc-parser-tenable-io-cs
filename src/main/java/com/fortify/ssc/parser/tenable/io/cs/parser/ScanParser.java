2032f7dd65034b729954487c54cfddf62dcb9a6c
Madhur  Jain <87946372+jmadhur87@users.noreply.github.com>
2026-08-11 16:25:21 +0530

    chore: Update parser-util version to latest (#16)
    fix: Upgrade dependencies

--- src/main/java/com/fortify/ssc/parser/tenable/io/cs/parser/ScanParser.java
+++ src/main/java/com/fortify/ssc/parser/tenable/io/cs/parser/ScanParser.java
@@ -4,16 +4,19 @@ import java.io.IOException;
 
 import com.fortify.plugin.api.ScanBuilder;
 import com.fortify.plugin.api.ScanData;
+import com.fortify.plugin.api.ScanEntry;
 import com.fortify.plugin.api.ScanParsingException;
 import com.fortify.util.jackson.IsoDateTimeConverter;
 import com.fortify.util.ssc.parser.json.ScanDataStreamingJsonParser;
 
 public class ScanParser {
 	private final ScanData scanData;
+	private final ScanEntry scanEntry;
     private final ScanBuilder scanBuilder;
     
-	public ScanParser(final ScanData scanData, final ScanBuilder scanBuilder) {
+	public ScanParser(final ScanData scanData, final ScanEntry scanEntry, final ScanBuilder scanBuilder) {
 		this.scanData = scanData;
+		this.scanEntry = scanEntry;
 		this.scanBuilder = scanBuilder;
 	}
 	
@@ -23,7 +26,7 @@ public class ScanParser {
 			.handler("/image_name", jp -> scanBuilder.setBuildId(jp.getValueAsString()))
 			.handler("/tag", jp -> scanBuilder.setScanLabel(jp.getValueAsString()))
 			.handler("/installed_packages", jp -> scanBuilder.setNumFiles(jp.countArrayEntries()))
-			.parse(scanData);
+			.parse(scanData, scanEntry);
 		scanBuilder.setEngineVersion("Unknown");
 		scanBuilder.completeScan();
 	}
